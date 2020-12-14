ee8f52b3ff0ed9ee71c57892e0015b3a23c83999
Zoee Leckron <leckronz@imsweb.com>
2020-12-14 14:52:40 -0500

    2424 added has_attachments boolean field for models in admin

--- attachments/mixin.py
+++ attachments/mixin.py
@@ -1,12 +1,17 @@
-
+from django import forms
 class AttachmentsAdminMixin:
 
     # label, slug, content_type
     # relation_name specifies the name cooresponding with the genericRelation
     relation_name = 'attachments'
     
-    def __init__():
-        self.list_display.update('has_attachments')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.list_display += ('has_attachments',)
+
+    def has_attachments(self, object):
+        return getattr(object, self.relation_name).exists()
+
 
 
 class AttachmentsAdminFormMixin:
@@ -14,8 +19,14 @@ class AttachmentsAdminFormMixin:
     # label, slug, content_type
     # relation_name specifies the name cooresponding with the genericRelation
     relation_name = 'attachments'
+    
 
-    def __init__():
-        pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['list_attachments'] = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True, 'readonly': True}))
+        
+
+
+
 
 
