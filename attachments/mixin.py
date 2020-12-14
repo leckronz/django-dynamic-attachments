88bf79e1d7e9727b7882497e9f580245988bd980
Brian Daisey <daiseyb@imsweb.com>
2020-12-14 16:20:43 -0500

    2424 - display attachments in admin as list of downloadable filenames

--- attachments/mixin.py
+++ attachments/mixin.py
@@ -1,4 +1,8 @@
+from django.conf import settings
 from django import forms
+from django.utils.html import format_html_join
+
+
 class AttachmentsAdminMixin:
 
     # label, slug, content_type
@@ -12,7 +16,14 @@ class AttachmentsAdminMixin:
     def has_attachments(self, object):
         return getattr(object, self.relation_name).exists()
 
+    def list_attachments(self, instance):
+        return format_html_join(
+            '\n', "<a href={}{}>{}</a><br>",
+            ((settings.BASE_URL, att.get_absolute_url(), att.file_name) for att in instance.attachments.all())
+        )
 
+    def get_readonly_fields(self, request, obj=None):
+        return super().get_readonly_fields(request, obj=obj) + ('list_attachments',)
 
 class AttachmentsAdminFormMixin:
 
